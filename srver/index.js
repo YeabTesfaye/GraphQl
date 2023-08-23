c7c70975e792ef97caf59288d43b5a1724024fd6
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-08-23 15:13:10 +0300

    added cors

--- srver/index.js
+++ srver/index.js
@@ -5,11 +5,12 @@ import "colors";
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema/schema.js";
 import { connectDB } from "./config/db.js";
+import cors from 'cors'
 dotev.config();
 const PORT = process.env.PORT | 5000;
 const app = express();
 connectDB()
-
+app.use(cors())
 app.use("/graphql", graphqlHTTP({
     schema,
     graphiql : process.env.NODE_ENV==='development',
